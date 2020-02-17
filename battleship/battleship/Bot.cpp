edd3ae262b30454d82f1bd0d45ed3d53235ed7ad
AlaskaBlake <59948466+AlaskaBlake@users.noreply.github.com>
2020-02-16 22:11:00 -0900

    worked on hardmode bot

--- battleship/battleship/Bot.cpp
+++ battleship/battleship/Bot.cpp
@@ -83,8 +83,100 @@ void Bot::placeShip() {
 
 }
 
+//edge cases
+//guesses randomly on edge of board
 bool Bot::shoot(Board& p2) {
 	if (_difficulty) {
+		for (int i = 0; i < _hits.size(); ++i) {
+			if (!(get<1>(_hits[i]))){
+				get<1>(_hits[i]) = true;
+				if (get<0>(_hits[i]) - 10 >= 0 && _guessBoard.getPos(get<0>(_hits[i]) - 10) != 'M' && _guessBoard.getPos(get<0>(_hits[i]) - 10) != 'H') {
+					if (p2.getPos(get<0>(_hits[i]) - 10) == 'S') {
+						p2.writeShot(get<0>(_hits[i]) - 10, 'H');
+							_guessBoard.writeShot(get<0>(_hits[i]) - 10, 'H');
+							_hits.push_back(make_tuple(get<0>(_hits[i]) - 10, false, false, true, false));
+							return true;
+					}
+					else {
+						p2.writeShot(get<0>(_hits[i]) - 10, 'M');
+							_guessBoard.writeShot(get<0>(_hits[i]) - 10, 'M');
+							return false;
+					}
+				}
+			}
+			else if (!(get<2>(_hits[i]))){
+				get<2>(_hits[i]) = true;
+				if (get<0>(_hits[i])/10 == (get<0>(_hits[i])+1) / 10 && _guessBoard.getPos(get<0>(_hits[i]) + 1) != 'M' && _guessBoard.getPos(get<0>(_hits[i]) + 1) != 'H') {
+					if (p2.getPos(get<0>(_hits[i]) + 1) == 'S') {
+						p2.writeShot(get<0>(_hits[i]) + 1, 'H');
+						_guessBoard.writeShot(get<0>(_hits[i]) + 1, 'H');
+						_hits.push_back(make_tuple(get<0>(_hits[i]) - 10, false, false, false, true));
+						return true;
+					}
+					else {
+						p2.writeShot(get<0>(_hits[i]) + 1, 'M');
+						_guessBoard.writeShot(get<0>(_hits[i]) + 1, 'M');
+						return false;
+					}
+				}
+			}
+			else if (!(get<3>(_hits[i]))) {
+				get<3>(_hits[i]) = true;
+				if (get<0>(_hits[i]) + 10 < 100 && _guessBoard.getPos(get<0>(_hits[i]) + 10) != 'M' && _guessBoard.getPos(get<0>(_hits[i]) + 10) != 'H') {
+					if (p2.getPos(get<0>(_hits[i]) + 10) == 'S') {
+						p2.writeShot(get<0>(_hits[i]) + 10, 'H');
+						_guessBoard.writeShot(get<0>(_hits[i]) + 10, 'H');
+						_hits.push_back(make_tuple(get<0>(_hits[i]) - 10, true, false, false, false));
+						return true;
+					}
+					else {
+						p2.writeShot(get<0>(_hits[i]) + 10, 'M');
+						_guessBoard.writeShot(get<0>(_hits[i]) + 10, 'M');
+						return false;
+					}
+				}
+			}
+			else if (!(get<4>(_hits[i]))){
+				get<4>(_hits[i]) = true;
+				if (get<0>(_hits[i]) / 10 == (get<0>(_hits[i]) - 1) / 10 && _guessBoard.getPos(get<0>(_hits[i]) - 1) != 'M' && _guessBoard.getPos(get<0>(_hits[i]) - 1) != 'H') {
+					if (p2.getPos(get<0>(_hits[i]) - 1) == 'S') {
+						p2.writeShot(get<0>(_hits[i]) - 1, 'H');
+						_guessBoard.writeShot(get<0>(_hits[i]) - 1, 'H');
+						_hits.push_back(make_tuple(get<0>(_hits[i]) - 10, false, true, false, false));
+						return true;
+					}
+					else {
+						p2.writeShot(get<0>(_hits[i]) - 1, 'M');
+						_guessBoard.writeShot(get<0>(_hits[i]) - 1, 'M');
+						return false;
+					}
+				}
+			}
+		}
+
+		while (true) {
+
+			auto package = getRowColDir();
+			char row = get<0>(package);
+			int col = get<1>(package);
+
+			int index = (row - 'A') * 10 + col;
+
+			if (index >= 0 && index <= 99 && _guessBoard.getPos(index) != 'M' && _guessBoard.getPos(index) != 'H') {
+				if (p2.getPos(index) == 'S') {
+					p2.writeShot(index, 'H');
+					_guessBoard.writeShot(index, 'H');
+					_hits.push_back(make_tuple(index, false, false, false, false));
+					return true;
+				}
+				else {
+					p2.writeShot(index, 'M');
+					_guessBoard.writeShot(index, 'M');
+					return false;
+				}
+
+			}
+		}
 
 	}
 	else {
